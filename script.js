8b825dd683717ea271a429dfade075bb70f279f7
saff-it <saff_it@ymail.com>
2022-06-21 17:57:09 +0200

    correction

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ const groceryContainer = document.querySelector('ul.gr-list-cont');
 
 let i = 0;
 
-while ( i < groceryList.length){
+while ( i < groceryList.length ){
     console.log(groceryList[i]);
 
     i++;
 
     let groceryItem = document.createElement('li');
-    groceryItem.innerHTML = groceryList[i];
+    groceryItem.innerHTML = groceryList[i-1];
 
     groceryContainer.append(groceryItem);
 
