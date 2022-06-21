2bcf96d3c94024411fabde8707aa3b013d9abd9a
saff-it <saff_it@ymail.com>
2022-06-21 15:18:08 +0200

    while cicle set up

---
+++ script.js
@@ -0,0 +1,25 @@
+
+const groceryList = [
+    'pane',
+    'acqua',
+    'uova',
+    'burro',
+    'olio',
+    'carne',
+    'tonno',
+    'pasta',
+]
+
+
+// for ( let i = 0; i < groceryList.length; i++) {
+//     console.log(groceryList[i]);
+
+// }
+
+let i = 0;
+
+while ( i < groceryList.length){
+    console.log(groceryList[i]);
+
+    i++;
+}
